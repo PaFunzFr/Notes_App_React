61ddd9bf97acea1991177b26b844abe3b1335562
funZ <153361103+PaFunzFr@users.noreply.github.com>
2026-01-21 14:32:32 +0100

    Create Note component

--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import {useState} from 'react';
 import TextInput from './inputs/TextInput';
 import SelectInput from './inputs/SelectInput';
--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Note from './Note';
 
 const NoteList = ({notes, deleteNote}) => {
     if (notes.length === 0) {
@@ -12,30 +13,11 @@ const NoteList = ({notes, deleteNote}) => {
     return (
         <div className="space-y-4">
             { notes.map( note => (
-                <div 
+                <Note 
                     key={note.id}
-                    className="p-4 bg-white rounded-lg shadow-md border-l-4"
-                >
-                    <h3 className="text-lg font-bold">
-                        {note.title}
-                    </h3>
-                    <p className="text-sm text-gray-600">
-                        <strong>Category: </strong> {note.category}
-                    </p>
-                    <p className="text-sm text-gray-600">
-                        <strong>Priority: </strong> {note.priority}
-                    </p>
-                    <p className="text-sm text-gray-600">
-                        <strong>Description: </strong> {note.description}
-                    </p>
-
-                    <button
-                        className="mt-3 text-red-500 cursor-pointer transition hover:text-red-700"
-                        onClick={() => deleteNote(note.id)}
-                    >
-                        🗑️ Delete
-                    </button>
-                </div>
+                    note={note}
+                    deleteNote={deleteNote}
+                />
             ))}
         </div>
     )
